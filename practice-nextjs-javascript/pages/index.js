b29867efc15ee2f6462fa25cb12d8d32ae661daa
exopas95 <exopas58@gmail.com>
2022-11-15 13:53:12 +0900

    [ADD] Movie API Connection

--- practice-nextjs-javascript/pages/index.js
+++ practice-nextjs-javascript/pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../componetns/Seo";
 
+const API_KEY = "0144f45a3703fb0b0b9bd176fee89e2b";
+
 export default function Home() {
+    const [movies, setMovies] = useState();
+    useEffect(() => {
+        (async () => {
+            const { results } = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
     return (
         <div>
             <Seo title="Home" />
-            <h1>Hello</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
     );
 }
